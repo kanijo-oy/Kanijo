6ca2dd90595f4e05881756c1abdc2d0a4e80dac0
Anssi Kolehmainen <anssi.kolehmainen@vilant.com>
2020-03-22 09:13:17 +0200

    Add simple help text

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,4 +8,7 @@ class HomeController < ApplicationController
 
   def policy
   end
+
+  def help
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :glocs
   get 'home/index'
   get 'home/policy'
+  get 'home/help'
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
