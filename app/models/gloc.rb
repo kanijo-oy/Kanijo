3bb8be5341b18b60a9f77081429958df940d756c
Anssi Kolehmainen <anssi.kolehmainen@vilant.com>
2020-03-22 09:44:50 +0200

    Show help text in main screen

--- app/models/gloc.rb
+++ app/models/gloc.rb
@@ -8,16 +8,24 @@ class Gloc < ApplicationRecord
   before_save :parse_upload
   def parse_upload
     return unless uploads.attached?
+    cc = 0
     uploads.blobs.each do |f|
       f.download do |dd|
         doc = Crack::XML.parse(dd)
-        doc['kml']['Document']['Placemark'].each do |place|
+        pms = doc.dig('kml', 'Document', 'Placemark')
+        unless pms
+          errors.add(:base, 'No places found in file')
+          next
+        end
+        pms.each do |place|
           next unless place.is_a? Hash
           next unless place.dig('TimeSpan', 'begin')
           ts = Time.parse(place['TimeSpan']['begin'])
           LogEntry.new(day: ts, time: ts, description: place['name'], user: user).save
+          cc += 1
         end
       end
     end
+    errors.add(:base, 'No places found in file') if cc == 0
   end
 end
