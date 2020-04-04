c279b9ddaa2fbf610ab678d758729d81beb86160
Anssi Kolehmainen <anssi@aketzu.net>
2020-04-04 20:28:39 +0300

    Misc cleanup

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,5 @@
 module HomeHelper
+  def timelineurl(day)
+    "https://www.google.fi/maps/timeline/kml?authuser=0&pb=!1m8!1m3!1i%u!2i%u!3i%u!2m3!1i%u!2i%u!3i%u" % [day.year, day.month-1, day.day, day.year, day.month-1, day.day]
+  end
 end
