ea8a0aed0e6be52f10ae0d92409e73f99eba9068
Anssi Kolehmainen <anssi@aketzu.net>
2020-04-13 17:28:13 +0300

    Add user name to profile

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -58,7 +58,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:phonenumber, :home_municipality])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:phonenumber, :home_municipality, :name])
   end
 
   # The path used after sign up.
