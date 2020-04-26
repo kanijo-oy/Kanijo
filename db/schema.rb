5ae5387a7d33b7220c5e73b19c60f554c677199c
Anssi Kolehmainen <anssi@aketzu.net>
2020-04-26 23:57:34 +0300

    Some new stuff

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_26_185852) do
+ActiveRecord::Schema.define(version: 2020_04_26_203942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,10 @@ ActiveRecord::Schema.define(version: 2020_04_26_185852) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
     t.string "contacts"
+    t.integer "duration"
+    t.string "placename"
+    t.string "address"
+    t.string "persons"
     t.index ["user_id"], name: "index_log_entries_on_user_id"
   end
 
