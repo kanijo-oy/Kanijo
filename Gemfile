b8d828d591f8e90738a4bc266ee944e919ad5895
Anssi Kolehmainen <anssi@aketzu.net>
2020-05-02 16:33:23 +0300

    Use Redis as session storage

--- Gemfile
+++ Gemfile
@@ -60,6 +60,8 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'active_storage_drag_and_drop'
 gem 'crack'
 gem 'devise'
+gem 'hiredis'
 gem 'omniauth-google-oauth2'
 gem 'prawn-rails'
 gem 'rails-i18n'
+gem 'redis'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.cache_store = :redis_cache_store, { url: 'redis://localhost:6379/1' }
+  config.session_store :cache_store
 end
